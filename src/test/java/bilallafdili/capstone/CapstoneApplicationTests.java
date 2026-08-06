4d706ce5f6ffe11d6a1b4d5d2b51dfe724619353
bilal lafdili <bilallafdili23@gmail.com>
2026-08-06 10:25:27 +0200

    Initial commit

---
+++ src/test/java/bilallafdili/capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package bilallafdili.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
